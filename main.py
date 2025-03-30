b96e3d7f018c2b6be92de1bb2289c70ce3ebfbcf
trexworld <lukas.kubin@gmx.at>
2025-03-30 17:54:57 +0200

    +added in get_sorted_chars_list the key:k,num:n list [dic]

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from stats import get_book_text, get_num_words, get_chars_dict
+from stats import get_book_text, get_num_words, get_chars_dict, get_sorted_chars_list
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
-    print(chars_dict)
+    chars_list = get_sorted_chars_list(chars_dict)
+
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {book_path}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------
+{chars_list}""")
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,11 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def get_sorted_chars_list(text):
+    new = []
+    for c in text:
+        list = [{"key" : c}]
+        list[0]["num"] = text[c]
+        new.append(list)
+    return new
